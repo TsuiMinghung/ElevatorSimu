3265c524ccf03c4fe80d36eb9eca638f65d95e54
alex <xmh727@gmail.com>
2023-04-01 12:27:50 +0800

    remove unnecessary code

--- Direction.java
+++ Direction.java
@@ -12,9 +12,6 @@ public enum Direction {
     }
 
     public boolean sameDirection(PersonRequest personRequest) {
-        if (personRequest == null) {
-            return false;
-        }
         if (this.equals(Direction.UP)) {
             return personRequest.getToFloor() > personRequest.getFromFloor();
         } else {
